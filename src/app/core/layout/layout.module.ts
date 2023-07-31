d915a4c2a5af43fd8032dcb66af78ca814701cf6
Marcin Lisiecki <marcinlisiecki9@gmail.com>
2023-07-31 15:20:47 +0200

    RIV-51 Add header link component & make logo a link

--- src/app/core/layout/layout.module.ts
+++ src/app/core/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { HeaderLinkComponent } from './header/header-link/header-link.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, HeaderLinkComponent],
+  exports: [HeaderComponent, HeaderLinkComponent],
   imports: [CommonModule, RouterLinkActive, RouterLink, NgOptimizedImage],
 })
 export class LayoutModule {}
