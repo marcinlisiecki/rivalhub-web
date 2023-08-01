a3f58e07824cc312afa29788aad770e62054da00
Zipperiusz <Zipperiusz0@gmail.com>
2023-08-01 10:14:08 +0200

    RIV-29 adding organization view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterFormComponent } from './features/auth/register-form/register-form.component';
+import { AddOrganizationComponent } from './features/organization/add-organization/add-organization.component';
 
 const routes: Routes = [
   {
     path: 'register',
     component: RegisterFormComponent,
   },
+  {
+    path: 'add-organization',
+    component: AddOrganizationComponent,
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './features/auth/auth.module';
-
+import { AddOrganizationComponent } from './features/organization/add-organization/add-organization.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AddOrganizationComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent],
