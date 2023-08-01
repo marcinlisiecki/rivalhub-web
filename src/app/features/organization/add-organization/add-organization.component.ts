633d7193e0b69cccf9c014e41a98a1023bf09436
Zipperiusz <Zipperiusz0@gmail.com>
2023-08-01 10:37:54 +0200

    RIV-29 fixing styles and formatting

--- src/app/features/organization/add-organization/add-organization.component.ts
+++ src/app/features/organization/add-organization/add-organization.component.ts
@@ -10,8 +10,8 @@ export class AddOrganizationComponent {
   uploadedFile: File | undefined;
   organizationName: string | undefined;
   imageURL: string = 'assets/img/organization.png';
-  error :string | undefined;
-  
+  error: string | undefined;
+
   onFileSelectClicked(event: FileSelectEvent) {
     this.uploadedFile = event.files[0];
     this.imageURL = URL.createObjectURL(event.currentFiles[0]);
@@ -23,14 +23,17 @@ export class AddOrganizationComponent {
   }
 
   onSendClick(organization: string) {
-    if(organization==undefined || organization.length<3 || organization.length>256)
-    {
-      this.error = "Nazwa organizacji musi posiadać od 2 do 256 znaków!";
+    if (
+      organization == undefined ||
+      organization.length < 3 ||
+      organization.length > 256
+    ) {
+      this.error = 'Nazwa organizacji musi posiadać od 2 do 256 znaków!';
       return;
     }
-    this.error=undefined;
-    URL.revokeObjectURL(this.imageURL)
-    this.organizationName=organization;
+    this.error = undefined;
+    URL.revokeObjectURL(this.imageURL);
+    this.organizationName = organization;
 
     //handle sending data to backend
   }
