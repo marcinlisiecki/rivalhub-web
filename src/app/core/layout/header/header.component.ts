20b431b76955a8a32e09fa7d4cf47d6951c936b7
Marcin Lisiecki <marcinlisiecki9@gmail.com>
2023-08-02 10:11:59 +0200

    RIV-43 Change header state depending on JWT token validity

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   isLoggedIn: boolean = false;
+
+  constructor(private authService: AuthService) {
+    this.authService.isAuthObservable().subscribe((val: boolean) => {
+      this.isLoggedIn = val;
+    });
+  }
 }
--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -5,20 +5,33 @@ import {
   LoginResponse,
   RegisterCredentials,
 } from '../../interfaces/auth';
-import { Observable, tap } from 'rxjs';
+import { Observable, Subject, tap } from 'rxjs';
 import { environment } from '../../../../environments/enviroment';
 import { CookieService } from 'ngx-cookie';
 import { JwtService } from '../jwt/jwt.service';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private authSubject!: Subject<boolean>;
+
   constructor(
     private http: HttpClient,
     private cookieService: CookieService,
     private jwtService: JwtService,
-  ) {}
+    private router: Router,
+  ) {
+    this.authSubject = new Subject<boolean>();
+    this.authSubject.next(this.isAuth());
+
+    this.router.events.subscribe((e) => {
+      if (e instanceof NavigationStart) {
+        this.authSubject.next(this.isAuth());
+      }
+    });
+  }
 
   register(credentials: RegisterCredentials): Observable<{}> {
     return this.http.post(environment.apiUrl + '/register', credentials);
@@ -31,11 +44,16 @@ export class AuthService {
         tap((result: any) => {
           if (result?.token) {
             this.cookieService.put('token', result.token);
-
-            return result;
           }
+
+          this.authSubject.next(this.isAuth());
+          return result;
         }),
       );
+  }
+
+  isAuthObservable(): Observable<boolean> {
+    return this.authSubject.asObservable();
   }
 
   isAuth(): boolean {
