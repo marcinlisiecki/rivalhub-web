eefe5d662fbf45965ad218f5c33d92ad28658e5f
Mateusz Szkudlarek <mateusz.szkudlarek55@gmail.com>
2023-08-02 15:56:29 +0200

    RIV-37 develop component

--- src/app/features/organization/invite-user/invite-user.component.ts
+++ src/app/features/organization/invite-user/invite-user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-invite-user',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class InviteUserComponent {
 
+  sendInviteForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email])
+  })
+
+  get email() {
+    return this.sendInviteForm.get('email')
+  }
+
+  onSubmit() {
+    // send email to recipient
+  }
 }
