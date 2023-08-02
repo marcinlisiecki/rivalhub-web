5ceb718b64cd25f4c43d8bebdeb835cc23cd5430
Marcin Lisiecki <marcinlisiecki9@gmail.com>
2023-08-02 16:16:24 +0200

    RIV-31 Connect new organization form with backend

--- src/app/features/organization/add-organization/add-organization.component.ts
+++ src/app/features/organization/add-organization/add-organization.component.ts
@@ -2,6 +2,10 @@ import { Component, Input } from '@angular/core';
 import { FileSelectEvent } from 'primeng/fileupload';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { subscribeOn } from 'rxjs';
+import { OrganizationsService } from '../../../core/services/organizations/organizations.service';
+import { Router } from '@angular/router';
+import { NewOrganization } from '../../../core/interfaces/Organization';
+import { extractMessage } from '../../../core/utils/apiErrors';
 
 @Component({
   selector: 'app-add-organization',
@@ -13,6 +17,11 @@ export class AddOrganizationComponent {
   imageURL: string = 'assets/img/avatars/avatarplaceholder.png';
   error: string | undefined;
 
+  constructor(
+    private organizationService: OrganizationsService,
+    private router: Router,
+  ) {}
+
   addForm = new FormGroup({
     name: new FormControl('', [
       Validators.required,
@@ -20,11 +29,12 @@ export class AddOrganizationComponent {
       Validators.maxLength(256),
     ]),
   });
+  apiError: null | string = null;
+  isLoading: boolean = false;
 
   onFileSelectClicked(event: FileSelectEvent) {
     this.uploadedFile = event.files[0];
     this.imageURL = URL.createObjectURL(event.currentFiles[0]);
-    console.log(this.imageURL);
   }
 
   onClearClicked(event: Event) {
@@ -33,14 +43,30 @@ export class AddOrganizationComponent {
   }
 
   onSubmit() {
+    this.apiError = null;
+
     if (!this.addForm.valid) {
       this.addForm.markAllAsTouched();
       return;
     }
 
+    this.isLoading = true;
     URL.revokeObjectURL(this.imageURL);
 
-    //handle sending data to backend
+    const newOrganization: NewOrganization = {
+      name: this.name?.value || '',
+    };
+
+    this.organizationService.add(newOrganization).subscribe({
+      next: () => {
+        this.router.navigateByUrl('/my-organizations').then();
+      },
+      error: (err: unknown) => {
+        this.apiError = extractMessage(err);
+      },
+    });
+
+    this.isLoading = false;
   }
 
   get name() {
