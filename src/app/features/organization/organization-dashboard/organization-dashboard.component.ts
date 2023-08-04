27acbb1ff15e0562e89593990a6d23551ce809d6
Zipperiusz <Zipperiusz0@gmail.com>
2023-08-04 10:19:47 +0200

    RIV-70 bring back mock data and css refactor

--- src/app/features/organization/organization-dashboard/organization-dashboard.component.ts
+++ src/app/features/organization/organization-dashboard/organization-dashboard.component.ts
@@ -15,9 +15,83 @@ import { ActivatedRoute } from '@angular/router';
 export class OrganizationDashboardComponent {
   navVisible: boolean = false;
   mobileView: boolean = window.innerWidth <= 768 ? true : false;
-  events!: EventDto[];
+  events: EventDto[] = [
+    {
+      id: 1,
+      name: 'Wędkowanie na jeziorze',
+      place: 'Jezioro',
+      date: new Date(),
+      beginEndTime: '10:00 - 14:00',
+      participantIds: [5],
+    },
+    {
+      id: 2,
+      name: 'Wędkowanie na rzece',
+      place: 'Rzeka',
+      date: new Date(),
+      beginEndTime: '10:00 - 14:00',
+      participantIds: [1, 2],
+    },
+    {
+      id: 3,
+      name: 'Wędkowanie na stawie',
+      place: 'Staw',
+      date: new Date(),
+      beginEndTime: '10:00 - 14:00',
+      participantIds: [2, 3, 4],
+    },
+  ];
   organization!: Organization;
-  users!: UserDto[];
+  users: UserDto[] = [
+    {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'JohnDoe@gmail.com',
+      imageUrl: 'assets/img/avatars/user.png',
+      role: 'Admin',
+    },
+    {
+      id: 2,
+      firstName: 'Jane',
+      lastName: 'Doe',
+      email: 'JaneDoe@gmail.com',
+      imageUrl: 'assets/img/avatars/user.png',
+      role: 'Member',
+    },
+    {
+      id: 3,
+      firstName: 'Jacob',
+      lastName: 'Dwayne',
+      email: 'JacobDwayne@gmail.com',
+      imageUrl: 'assets/img/avatars/user.png',
+      role: 'Member',
+    },
+    {
+      id: 4,
+      firstName: 'Albert',
+      lastName: 'Smart',
+      email: 'AlbertSmart@gmail.com',
+      imageUrl: 'assets/img/avatars/user.png',
+      role: 'Member',
+    },
+    {
+      id: 5,
+      firstName: 'Alice',
+      lastName: 'Rabbit',
+      email: 'AliceRabbit@gmail.com',
+      imageUrl: 'assets/img/avatars/user.png',
+      role: 'Member',
+    },
+    {
+      id: 6,
+      firstName: 'Mark',
+      lastName: 'Parker',
+      email: 'MarkParker@gmail.com',
+      imageUrl: 'assets/img/avatars/user.png',
+      role: 'Member',
+    },
+  ];
   id!: number;
 
   constructor(
