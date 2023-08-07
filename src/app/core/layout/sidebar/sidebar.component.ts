a753a70efad962f20815e1db1b320240d0031b08
Zipperiusz <Zipperiusz0@gmail.com>
2023-08-07 16:01:42 +0200

    RIV-69 sidebar mobile view

--- src/app/core/layout/sidebar/sidebar.component.ts
+++ src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,26 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   navBtnAnimation,
   navBtnAnimationMobile,
 } from 'src/app/core/animations/sidebar-animation';
 import { ViewService } from '@app/core/services/view/view.service';
+import { AuthService } from '@app/core/services/auth/auth.service';
+import { UserDetailsDto } from '@app/core/interfaces/UserDetailsDto';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
   animations: [navBtnAnimation, navBtnAnimationMobile],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   sidebarVisible: boolean = false;
-  mobileView!: boolean;
+  isLoggedIn: boolean = true;
 
-  constructor(private viewService: ViewService) {}
+  mobileView!: boolean;
+  selectedOrganization: string = 'NCDC';
+  user: UserDetailsDto = {
+    id: 0,
+    name: 'Dominik Matuszewski',
+    email: 'zippek@edu.pl',
+    profilePictureUrl: 'https://i.imgur.com/1qB7B9F.png',
+  };
+
+  constructor(
+    private viewService: ViewService,
+    private authService: AuthService,
+  ) {}
 
   ngOnInit() {
     this.mobileView = this.viewService.mobileView;
     this.viewService.resizeEvent.subscribe((value: boolean) => {
       this.mobileView = value;
     });
+
+    this.authService.isAuthObservable().subscribe((val: boolean) => {
+      this.isLoggedIn = val;
+    });
+  }
+
+  logout() {
+    this.authService.logout();
   }
 
   toggleSidebar() {
