36955d411fe0134c646df86e491ca23afac847d6
Piotrek Kaczalko <piotr@kaczalko.pl>
2023-08-11 23:04:08 +0200

    RIV-93 Switching library from angular-calendar to fullcalendar

--- src/app/features/calendar/calendar-body/calendar-body.component.ts
+++ src/app/features/calendar/calendar-body/calendar-body.component.ts
@@ -1,41 +1,89 @@
-import {ChangeDetectionStrategy, Inject,Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
-import {CalendarEvent, CalendarView} from "angular-calendar";
-import { DOCUMENT } from '@angular/common';
+import { Component, signal, ChangeDetectorRef } from '@angular/core';
+import {
+  CalendarOptions,
+  DateSelectArg,
+  EventClickArg,
+  EventApi,
+} from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import { INITIAL_EVENTS, createEventId } from './event-utils';
 
 @Component({
   selector: 'app-calendar-body',
   templateUrl: './calendar-body.component.html',
   styleUrls: ['./calendar-body.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None,
 })
-export class CalendarBodyComponent implements OnInit, OnDestroy{
-  view: CalendarView = CalendarView.Month;
-
-  viewDate = new Date();
-
-  events: CalendarEvent[] = [
-    {
-      title: 'LOL',
-      start: new Date(),
+export class CalendarBodyComponent {
+  calendarVisible = signal(true);
+  calendarOptions = signal<CalendarOptions>({
+    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
     },
-  ];
+    initialView: 'dayGridMonth',
+    initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    eventsSet: this.handleEvents.bind(this),
+    /* you can update a remote database when these fire:
+    eventAdd:
+    eventChange:
+    eventRemove:
+    */
+  });
+  currentEvents = signal<EventApi[]>([]);
 
-  lol(){
-    alert("lol");
+  constructor(private changeDetector: ChangeDetectorRef) {}
+
+  handleCalendarToggle() {
+    this.calendarVisible.update((bool) => !bool);
   }
 
-  private readonly darkThemeClass = 'dark-theme';
-
-
-  constructor(@Inject(DOCUMENT) private document:any) {}
-
-  ngOnInit(): void {
-    this.document.body.classList.add(this.darkThemeClass);
+  handleWeekendsToggle() {
+    this.calendarOptions.mutate((options) => {
+      options.weekends = !options.weekends;
+    });
   }
 
-  ngOnDestroy(): void {
-    this.document.body.classList.remove(this.darkThemeClass);
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
   }
 
+  handleEventClick(clickInfo: EventClickArg) {
+    if (
+      confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`,
+      )
+    ) {
+      clickInfo.event.remove();
+    }
+  }
+
+  handleEvents(events: EventApi[]) {
+    this.currentEvents.set(events);
+    this.changeDetector.detectChanges(); // workaround for pressionChangedAfterItHasBeenCheckedError
+  }
 }
--- src/app/features/calendar/calendar.module.ts
+++ src/app/features/calendar/calendar.module.ts
@@ -1,35 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarEventsComponent } from '@app/features/calendar/calendar-events/calendar-events.component';
-import { CalendarComponent } from "@app/features/calendar/calendar.component";
+import { CalendarComponent } from '@app/features/calendar/calendar.component';
 import { CalendarBodyComponent } from './calendar-body/calendar-body.component';
-import { CalendarModule as AngularCalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import {MenuModule} from "primeng/menu";
+import { MenuModule } from 'primeng/menu';
 import { CalendarFilterComponent } from './calendar-filter/calendar-filter.component';
-import {SharedModule} from "@app/shared/shared.module";
-
+import { SharedModule } from '@app/shared/shared.module';
+import { FullCalendarModule } from '@fullcalendar/angular';
 
 @NgModule({
   declarations: [
     CalendarEventsComponent,
     CalendarComponent,
     CalendarBodyComponent,
-    CalendarFilterComponent
+    CalendarFilterComponent,
   ],
-  imports: [
-    CommonModule,
-    AngularCalendarModule.forRoot({
-      provide: DateAdapter,
-      useFactory: adapterFactory,
-    }),
-    MenuModule,
-    SharedModule,
-  ],
-  exports: [
-    CalendarEventsComponent,
-    CalendarComponent,
-    CalendarBodyComponent
-  ]
+  imports: [CommonModule, MenuModule, SharedModule, FullCalendarModule],
+  exports: [CalendarEventsComponent, CalendarComponent, CalendarBodyComponent],
 })
-export class CalendarModule { }
+export class CalendarModule {}
