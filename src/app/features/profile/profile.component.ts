97af531821a14aa93a00e05af7c4f8fcc659d582
Zipperiusz <Zipperiusz0@gmail.com>
2023-08-11 12:30:59 +0200

    RIV-115 user service

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,22 +1,41 @@
-import { Component } from '@angular/core';
-import {UserDetailsDto} from "@interfaces/user/user-details-dto";
-import {Reservation} from "@interfaces/reservation/reservation";
-import {EVENTS, RESERVATIONS} from "@app/mock/stations";
-import {EventDto} from "@interfaces/event/event-dto";
-
+import { Component, OnInit } from '@angular/core';
+import { UserDetailsDto } from '@interfaces/user/user-details-dto';
+import { Reservation } from '@interfaces/reservation/reservation';
+import { EVENTS, RESERVATIONS } from '@app/mock/stations';
+import { EventDto } from '@interfaces/event/event-dto';
+import { UserService } from '@app/core/services/user/user.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {
-  user: UserDetailsDto = {
-    id: 1,
-    name: "Jakub Buszta",
-    email: "buszta-jakub@wp.pl",
-    profilePictureUrl: "url"
-  };
+export class ProfileComponent implements OnInit {
+  user!: UserDetailsDto;
   reservations: Reservation[] = RESERVATIONS;
   events: EventDto[] = EVENTS;
 
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    const userId = this.route.snapshot.params['id'];
+
+    this.userService.getUser(userId).subscribe((user) => {
+      console.log(user);
+      this.user.name = user.name;
+      this.user.email = user.email;
+      this.user.profilePictureUrl = this.checkAvatar(user.profilePictureUrl);
+    });
+  }
+
+  private checkAvatar(url: string): string {
+    if (url === null) {
+      return 'https://www.gravatar.com/avatar/0';
+    } else {
+      return url;
+    }
+  }
 }
