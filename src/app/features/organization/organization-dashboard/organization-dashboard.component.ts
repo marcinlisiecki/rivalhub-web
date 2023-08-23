7c68cfd41cb368e15a11cc026dda6eeb670eb1b2
Mateusz Szkudlarek <mateusz.szkudlarek55@gmail.com>
2023-08-23 12:27:11 +0200

    RIV-198 toast message added on successfully sent email and toast message on failure

--- src/app/features/organization/organization-dashboard/organization-dashboard.component.ts
+++ src/app/features/organization/organization-dashboard/organization-dashboard.component.ts
@@ -71,7 +71,7 @@ export class OrganizationDashboardComponent implements OnInit, OnDestroy {
       this.messageService.add({
         severity: 'success',
         life: 1000 * 10,
-        summary: 'Pomyślnie skonfigurowao organizację',
+        summary: 'Pomyślnie skonfigurowano organizację',
       });
       this.router
         .navigate([], {
@@ -80,6 +80,21 @@ export class OrganizationDashboardComponent implements OnInit, OnDestroy {
         })
         .then();
     }
+
+    const inviteSuccess = this.route.snapshot.queryParams['invited']
+    if (inviteSuccess) {
+      this.messageService.add({
+        severity: 'success',
+        life: 1000 * 10,
+        summary: 'Pomyślnie wysłano zaproszenie',
+      });
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: {}
+      })
+        .then()
+    }
+
     this.amIAdmin = this.authService.amIAdmin(this.id);
     this.getOrganizationEvents();
   }
