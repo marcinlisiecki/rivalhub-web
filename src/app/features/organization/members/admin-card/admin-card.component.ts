090b968cc24c3540413c16b2a3663dd2fbe192eb
Zipperiusz <Zipperiusz0@gmail.com>
2023-08-25 16:58:03 +0200

    RIV-212 profile images on other vievs

--- src/app/features/organization/members/admin-card/admin-card.component.ts
+++ src/app/features/organization/members/admin-card/admin-card.component.ts
@@ -1,43 +1,56 @@
-import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
-import {UserDetailsDto} from "@interfaces/user/user-details-dto";
-import {ConfirmationService} from "primeng/api";
-import {OrganizationsService} from "@app/core/services/organizations/organizations.service";
-import {ActivatedRoute, Router} from "@angular/router";
-import {AuthService} from "@app/core/services/auth/auth.service";
-import {HttpErrorResponse} from "@angular/common/http";
-import {take} from "rxjs";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { UserDetailsDto } from '@interfaces/user/user-details-dto';
+import { ConfirmationService } from 'primeng/api';
+import { OrganizationsService } from '@app/core/services/organizations/organizations.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '@app/core/services/auth/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { take } from 'rxjs';
+import { ImageService } from '@app/core/services/image/image.service';
 
 @Component({
   selector: 'app-admin-card',
   templateUrl: './admin-card.component.html',
-  styleUrls: ['./admin-card.component.scss']
+  styleUrls: ['./admin-card.component.scss'],
 })
 export class AdminCardComponent implements OnInit {
+  @Input() user!: UserDetailsDto;
+  @Input() amIAdmin: boolean = false;
+  @Output() changedStatus = new EventEmitter<void>();
 
-  @Input() user!: UserDetailsDto
-  @Input() amIAdmin: boolean = false
-  @Output() changedStatus = new EventEmitter<void>()
-
-  private organizationId!: number
-  private confirmKickMessage: string = "Na pewno chcesz usunąć tego członka?"
-  private unAdminMessage: string = "Na pewno chcesz odebrać admina temu członkowi?"
-  public adminButtonText: string = "Odbierz admina"
-  public kickButtonText: string = "Wyrzuć";
-  public myself: boolean = false
-
+  private organizationId!: number;
+  private confirmKickMessage: string = 'Na pewno chcesz usunąć tego członka?';
+  private unAdminMessage: string =
+    'Na pewno chcesz odebrać admina temu członkowi?';
+  public adminButtonText: string = 'Odbierz admina';
+  public kickButtonText: string = 'Wyrzuć';
+  public myself: boolean = false;
+  imageUrl!: string;
   constructor(
     private route: ActivatedRoute,
     private confirmationService: ConfirmationService,
     private organizationService: OrganizationsService,
     private router: Router,
     private authService: AuthService,
+    private imageService: ImageService,
   ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.organizationId = params['id'];
     });
-    this.loadMyself()
+    this.loadMyself();
+
+    this.imageUrl = this.imageService.getUserImagePath(
+      this.user.profilePictureUrl,
+    );
   }
 
   getImagePath(imageUrl: string | null): string {
@@ -50,7 +63,7 @@ export class AdminCardComponent implements OnInit {
 
   loadMyself() {
     if (this.authService.getUserId() == this.user.id) {
-      this.myself = true
+      this.myself = true;
     }
   }
 
@@ -62,16 +75,20 @@ export class AdminCardComponent implements OnInit {
       rejectLabel: 'Nie',
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
-        this.organizationService.kickUser(this.organizationId, this.user.id).subscribe({
-          next: response => {
-            this.changedStatus.emit()
-          }
-        })
-        this.organizationService.unAdmin(this.organizationId, this.user.id).subscribe({
-          next: response => {
-            console.log("git")
-          }
-        })
+        this.organizationService
+          .kickUser(this.organizationId, this.user.id)
+          .subscribe({
+            next: (response) => {
+              this.changedStatus.emit();
+            },
+          });
+        this.organizationService
+          .unAdmin(this.organizationId, this.user.id)
+          .subscribe({
+            next: (response) => {
+              console.log('git');
+            },
+          });
       },
       reject: () => {},
     });
@@ -96,21 +113,23 @@ export class AdminCardComponent implements OnInit {
       rejectLabel: 'Nie',
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
-        this.organizationService.unAdmin(this.organizationId, this.user.id).subscribe({
-          next: response => {
-            this.changedStatus.emit()
-          },
-          error: HttpErrorResponse => {
-            console.log('nie git')
-          }
-        })
+        this.organizationService
+          .unAdmin(this.organizationId, this.user.id)
+          .subscribe({
+            next: (response) => {
+              this.changedStatus.emit();
+            },
+            error: (HttpErrorResponse) => {
+              console.log('nie git');
+            },
+          });
       },
-      reject: () => {}
-    })
+      reject: () => {},
+    });
   }
 
   takeToUserProfile() {
-    this.router.navigateByUrl(`profiles/${this.user.id}`).then()
+    this.router.navigateByUrl(`profiles/${this.user.id}`).then();
   }
 
   protected readonly take = take;
--- src/app/features/profile/dashboard-profile/dashboard-profile.component.ts
+++ src/app/features/profile/dashboard-profile/dashboard-profile.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { headerCompactAnimation } from '@app/core/animations/header-animation';
+import { ImageService } from '@app/core/services/image/image.service';
 import { Organization } from '@interfaces/organization/organization';
 import { UserDetailsDto } from '@interfaces/user/user-details-dto';
 
@@ -9,22 +10,23 @@ import { UserDetailsDto } from '@interfaces/user/user-details-dto';
   templateUrl: './dashboard-profile.component.html',
   styleUrls: ['./dashboard-profile.component.scss'],
 })
-export class DashboardProfileComponent {
+export class DashboardProfileComponent implements OnInit {
   @Input({ required: true }) user!: UserDetailsDto;
   @Input() compact: boolean = false;
   @Input() isMe!: boolean;
+  imageUrl!: string;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private imageService: ImageService,
+  ) {}
+  ngOnInit(): void {
+    this.imageUrl = this.imageService.getUserImagePath(
+      this.user.profilePictureUrl,
+    );
+  }
 
   goToEdit(): void {
     this.router.navigateByUrl(`/profiles/${this.user.id}/edit`).then();
-  }
-
-  checkAvatar(url: string): string {
-    if (url === null) {
-      return 'https://www.gravatar.com/avatar/0';
-    } else {
-      return url;
-    }
   }
 }
