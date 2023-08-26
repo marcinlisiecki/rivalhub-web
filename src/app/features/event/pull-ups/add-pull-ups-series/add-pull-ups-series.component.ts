a0a1f41088fc58a2e2b6f178baeab93cd2d7afb2
Marcin Lisiecki <marcinlisiecki9@gmail.com>
2023-08-26 11:36:51 +0200

    RIV-169 validate sets & series

--- src/app/features/event/pull-ups/add-pull-ups-series/add-pull-ups-series.component.ts
+++ src/app/features/event/pull-ups/add-pull-ups-series/add-pull-ups-series.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { UserDetailsDto } from '@interfaces/user/user-details-dto';
 import { AddPullUpsUserResults } from '@interfaces/event/games/pull-ups/add-pull-ups-user-results';
+import { LanguageService } from '@app/core/services/language/language.service';
 
 @Component({
   selector: 'app-add-pull-ups-series',
@@ -13,10 +14,12 @@ export class AddPullUpsSeriesComponent {
   matchId!: number;
   users!: UserDetailsDto[];
   usersResults: AddPullUpsUserResults[] = [];
+  errorMessage?: string;
 
   constructor(
     private dialogConfig: DynamicDialogConfig,
     private dialogRef: DynamicDialogRef,
+    private languageService: LanguageService,
   ) {
     this.seriesID = dialogConfig.data['seriesID'];
     this.matchId = dialogConfig.data['matchId'];
@@ -33,6 +36,28 @@ export class AddPullUpsSeriesComponent {
   }
 
   onSubmit() {
+    let isValid = true;
+
+    this.usersResults.forEach((result) => {
+      if (result.result < 0) {
+        this.errorMessage = this.languageService.instant(
+          'event.series.validation.requirePositiveNumber',
+        );
+        isValid = false;
+      }
+
+      if (result.result > 10_000) {
+        this.errorMessage = this.languageService.instant(
+          'event.series.validation.tooBig',
+        );
+        isValid = false;
+      }
+    });
+
+    if (!isValid) {
+      return;
+    }
+
     this.dialogRef.close({
       matchId: this.matchId,
       series: this.usersResults,
